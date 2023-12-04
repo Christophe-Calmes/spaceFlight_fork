f0c6a5ecb3e9045f56497e4ad4dd859555098183
Calmes <christophe.calmes@laplateforme.io>
2023-12-04 15:31:16 +0100

    ready, not ready for takeoff

--- src/pages/MeteoSites/CardLaunchPlace.jsx
+++ src/pages/MeteoSites/CardLaunchPlace.jsx
@@ -6,16 +6,21 @@ const cardLaunchPlace = ({dataWeather, index}) => {
     {nameSpaceCenter: 'Baïkonour', illustration: 'baikonourSpaceCenter.jpg' },
     {nameSpaceCenter: 'Juiquan', illustration: 'jiuquan.jpg' },
   ];
-  
+  let launchOk = false;
+if(dataWeather?.wind10m_max <= 4) {
+  launchOk = true;
+}
+
+
   return (
     <div className="item">
           <ul>
-            <li><h2>{pictureBank[index].nameSpaceCenter}</h2></li>
+            <li><h2>{pictureBank[index].nameSpaceCenter}</h2>{launchOk ? (<h3>Launch Pad ready for takeoff</h3>):(<h3>Launch Pad not ready for takeoff</h3>)}</li>
             <img className="LaunchPadPicture" src={`./src/assets/PicturesBank/${pictureBank[index].illustration}`} alt={pictureBank[index].nameSpaceCenter}/>
             <li>Weather : {dataWeather?.weather}</li>
             <li>Temperature maximum : {dataWeather?.temp2m?.max} °C</li>
-            <li>Temperature minimum : {dataWeather?.temp2m?.max} °C</li>
-            <li>Wind max at 10 m : {dataWeather?.wind10m_max} m/s / {dataWeather?.wind10m_max * 3.6} km/h </li>
+            <li>Temperature minimum : {dataWeather?.temp2m?.min} °C</li>
+            <li>Wind max at 10 m : {dataWeather?.wind10m_max} m/s | {dataWeather?.wind10m_max * 3.6} km/h </li>
           </ul>
     </div>
   )
